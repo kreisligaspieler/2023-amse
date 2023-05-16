e2fe3133482205a059d11c6d721d118d7deb1136
Elias <elias.pfann@fau.de>
2023-05-17 01:59:35 +0200

    getData & project-plan updated

--- data/getData.py
+++ data/getData.py
@@ -1,38 +1,23 @@
-import pandas
+"""Downloads datasets and writes them into a sqlite-database. All datasets containing chlorophyll a measurements are stored in one table.
+The datasets containing temperature measurements are stored in a separate table. To get the database, install all requirements from requirements.txt and execute this file. 
+Tested successfully with python3. 10.6."""
 import requests
-from lxml import html
 import sqlite3
+import os
 
-url1 = 'https://data.bsh.de/OpenData/DOD/MO_H_CHLA/'
-url2 = 'https://data.bsh.de/OpenData/DOD/MO_H_TEMP/'
-# dynamicly download all available datasets
-# get webpage
-r1 = requests.get(url1, allow_redirects=True)
-source1 = html.fromstring(r1.content)
-r2 = requests.get(url2, allow_redirects=True)
-source2 = html.fromstring(r2.content)
-# extract links
-links1 = source1.xpath('//a/@href')
-links2 = source2.xpath('//a/@href')
-# filter dataset links
-datasets1= [x for x in links1 if x.startswith("MO")]
-datasets2= [x for x in links2 if x.startswith("MO")]
-# download each dataset and process it
-relative_path = "/home/elias/git/2023-amse/data/"
-for d in datasets1:
+def download_and_process(url, statement, type):
+    relative_path = "/home/elias/git/2023-amse/data/"
     # download dataset
-    r = requests.get(url1+d, allow_redirects=True)
-    open(relative_path+d, 'wb').write(r.content)
-     # connect to database
+    r = requests.get(url, allow_redirects=True)
+    open(relative_path+"tmp.txt", 'wb').write(r.content)
+    # connect to database
     con = sqlite3.connect(relative_path+"database.sqlite")
     cur = con.cursor()
-    # remove comments, fix format of date column 
-    filename=d.removesuffix(".txt")
-    file = open(relative_path+d,'r', encoding='ISO-8859-15')
-    # create table, datasets
-    cur.execute("CREATE TABLE IF NOT EXISTS "+ filename + " (Cruise VARCHAR(255), Station VARCHAR(255), Type VARCHAR(10), Date TEXT, [Longitude degrees_east] REAL, [Latitude degrees_north] REAL, LOCAL_CDI_ID VARCHAR(255), EDMO_code VARCHAR(10), [Bot Depth in m] FLOAT, [Depth in m] FLOAT, QV INTEGER, [CHLA_FLM_FGN_ALC in µg/l] FLOAT, QV_1 INTEGER, [CHLA_PTM_F_ACE in µg/l]	FLOAT, QV_2 INTEGER, [CHLA_SENE_DEV_NDT in µg/l] FLOAT, QV_3 INTEGER, [CHLA_SENE_NOT_NDT in  µg/l] FLOAT, QV_4 INTEGER)")
+    file = open(relative_path+"tmp.txt",'r', encoding='ISO-8859-15')
+    # create table
+    cur.execute(statement)
     for line in file.readlines():
-        if not (line.startswith('//')) and not line.isspace() and not line.startswith("Cruise"):
+        if not (line.startswith('//')) and not line.isspace() and not line.startswith("Cruise"): # skip comments, empty lines and heading
             # replace T in date and write line to database
             date_old = line.split("\t")[3]
             date_new = date_old.replace("T"," ")
@@ -40,16 +25,43 @@ for d in datasets1:
             # prepare line for sql statement
             count = line.count("\t") - 1
             line=line.replace(",", ".").replace("\t", "','", count)
-            # if last vales are empty or dataset < 2020 -> padd with empty entries
-            while count < 18:
-                line=line+"','"
-                count+=1
-            line="'"+line+"'"
-            cur.execute("INSERT INTO "+ filename+" VALUES("+line+");")
+            if(type=="CHLA"):
+                # if last vales are empty or dataset = 2018 -> padd with empty entries
+                while count < 18:
+                    line=line+"','"
+                    count+=1
+                line="'"+line+"'"
+            else: 
+                # if last vales are empty -> padd with empty entries
+                while count < 14:
+                    line=line+"','"
+                    count+=1
+                line="'"+line+"'"
+            # insert line into database
+            cur.execute("INSERT INTO CHLA VALUES("+line+");")
             con.commit()
+    cur.close()         
     cur.close()
     con.close()
     file.close()
+    # cleanup 
+    os.remove(relative_path+"tmp.txt")
+    
+
+url1 = 'https://data.bsh.de/OpenData/DOD/MO_H_CHLA/MO_H_CHLA_2018.txt'
+url2 = 'https://data.bsh.de/OpenData/DOD/MO_H_CHLA/MO_H_CHLA_2022.txt'
+url3 = 'https://data.bsh.de/OpenData/DOD/MO_H_TEMP/MO_H_TEMP_2018.txt'
+url4 = 'https://data.bsh.de/OpenData/DOD/MO_H_TEMP/MO_H_TEMP_2022.txt'
+# download CHLA-datasets and process them
+# input-params: url of dataset, sql statement for generating table, type of dataset (CHLA or temp)  
+statement_CHLA="CREATE TABLE IF NOT EXISTS CHLA (Cruise VARCHAR(255), Station VARCHAR(255), Type VARCHAR(10), Date TEXT, [Longitude degrees_east] REAL, [Latitude degrees_north] REAL, LOCAL_CDI_ID VARCHAR(255), EDMO_code VARCHAR(10), [Bot Depth in m] FLOAT, [Depth in m] FLOAT, QV INTEGER, [CHLA_FLM_FGN_ALC in µg/l] FLOAT, QV_1 INTEGER, [CHLA_PTM_F_ACE in µg/l]	FLOAT, QV_2 INTEGER, [CHLA_SENE_DEV_NDT in µg/l] FLOAT, QV_3 INTEGER, [CHLA_SENE_NOT_NDT in  µg/l] FLOAT, QV_4 INTEGER)"
+statement_TEMP="CREATE TABLE IF NOT EXISTS TEMP (Cruise VARCHAR(255), Station VARCHAR(255), Type VARCHAR(10), Date TEXT, [Longitude degrees_east] REAL, [Latitude degrees_north] REAL, LOCAL_CDI_ID VARCHAR(255), EDMO_code VARCHAR(10), [Bot Depth in m] FLOAT, [Depth in m] FLOAT, QV INTEGER, [CHLA_FLM_FGN_ALC in µg/l] FLOAT, QV_1 INTEGER, [TEMP_THEPRC_DEV_NDT[°C]]	FLOAT, QV_2 INTEGER)"
+download_and_process(url1, statement_CHLA, "CHLA")
+download_and_process(url2, statement_CHLA, "CHLA")
+download_and_process(url3, statement_TEMP, "TEMP")
+download_and_process(url4, statement_TEMP, "TEMP")
+
+
     
 
    
--- project/project-plan.md
+++ project/project-plan.md
@@ -2,7 +2,7 @@
 
 ## Summary
 
-This project analyzes the correlation between chlorophyll a in seawater which indicates changes in marine plants like phytoplankton and seawater temperature in the North and Baltic Seas of Germany. Data from 2017 to the present are analyzed to detect changes as well in chlorophyll a and temperature. 
+This project analyzes the correlation between chlorophyll a in seawater which indicates changes in marine plants like phytoplankton and seawater temperature in the North and Baltic Seas of Germany. Data from 2018 and 2022 are analyzed to detect changes as well in chlorophyll a and temperature.
 ## Rationale
 
 This analysis helps detect the growth or reduction of marine plants, which are important for the maritime ecosystem, e.g., as food for animals and also for producing oxygen. It also shows the possible effect of water temperature on marine plants levels. This could help identify the reasons for the decline in animal populations as these animals eat marine plants as well as changes in CO<sub>2</sub> concentrations which might be affected by a change in the amount of CO<sub>2</sub> processing plants in the sea.
@@ -10,21 +10,23 @@ This analysis helps detect the growth or reduction of marine plants, which are i
 
 ### Datasource1: Chlorophyll-a im Meerwasser - Serie 
 * Metadata URL: https://mobilithek.info/offers/-7508543878210012949
-* Data URL: https://www.geoseaportal.de/atomfeeds/MO_H_CHLA_de.xml
+* Data URL: https://data.bsh.de/OpenData/DOD/MO_H_CHLA/
 * Data Type: txt
 
-Level of chlorophyll a in the North and Baltic Seas of Germany from 2017 till now.
+Level of chlorophyll a in the North and Baltic Seas of Germany from 2017 till now. 
+License: GeoSeaPortal des BSH, licensed under the following license: Data licence Germany – attribution – Version 2.0" or "dl-de/by-2-0" referring to the licence text available at www.govdata.de/dl-de/by-2-0, data available at https://data.bsh.de/OpenData/DOD/MO_H_CHLA/ 
 
 ### Datasource2: Temperatur des Meerwassers - Serie 
 * Metadata URL: https://mobilithek.info/offers/-832924192195927122
-* Data URL: https://www.geoseaportal.de/atomfeeds/MO_H_TEMP_de.xml
+* Data URL: https://data.bsh.de/OpenData/DOD/MO_H_TEMP/
 * Data Type: txt  
 
 Seawater temperature in the North and Baltic Seas of Germany from 2017 till now.
+License: GeoSeaPortal des BSH, licensed under the following license: Data licence Germany – attribution – Version 2.0" or "dl-de/by-2-0" referring to the licence text available at www.govdata.de/dl-de/by-2-0, data available at https://data.bsh.de/OpenData/DOD/MO_H_TEMP/
 
 ## Work Packages
 
-1. Jupyter notebook. [#1](https://github.com/kreisligaspieler/2023-amse/issues/4) 
-2. Import data sources [#2](https://github.com/kreisligaspieler/2023-amse/issues/1) 
+1. Import data sources [#1](https://github.com/kreisligaspieler/2023-amse/issues/1) 
+2. Jupyter notebook. [#2] (https://github.com/kreisligaspieler/2023-amse/issues/4) 
 3. Calculate data and gernerate graphs [#3](https://github.com/kreisligaspieler/2023-amse/issues/2)
 4. Evaluation [#4](https://github.com/kreisligaspieler/2023-amse/issues/3) 
