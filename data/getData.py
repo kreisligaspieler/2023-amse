c569ce97a4af2d0bdee64759c75d15a71bf17a00
kreisligaspieler <e.pfann@gmx.de>
2023-05-17 04:14:57 +0200

    getData

--- data/getData.py
+++ data/getData.py
@@ -9,11 +9,11 @@ def download_and_process(url, statement, type):
     relative_path = "/home/elias/git/2023-amse/data/"
     # download dataset
     r = requests.get(url, allow_redirects=True)
-    open(relative_path+"tmp.txt", 'wb').write(r.content)
+    open(relative_path+type, 'wb').write(r.content)
     # connect to database
     con = sqlite3.connect(relative_path+"database.sqlite")
     cur = con.cursor()
-    file = open(relative_path+"tmp.txt",'r', encoding='ISO-8859-15')
+    file = open(relative_path+type,'r', encoding='ISO-8859-15')
     # create table
     cur.execute(statement)
     for line in file.readlines():
@@ -38,14 +38,13 @@ def download_and_process(url, statement, type):
                     count+=1
                 line="'"+line+"'"
             # insert line into database
-            cur.execute("INSERT INTO CHLA VALUES("+line+");")
+            cur.execute("INSERT INTO "+ type + " VALUES("+line+");")
             con.commit()
-    cur.close()         
     cur.close()
     con.close()
     file.close()
     # cleanup 
-    os.remove(relative_path+"tmp.txt")
+    os.remove(relative_path+type)
     
 
 url1 = 'https://data.bsh.de/OpenData/DOD/MO_H_CHLA/MO_H_CHLA_2018.txt'
@@ -55,7 +54,8 @@ url4 = 'https://data.bsh.de/OpenData/DOD/MO_H_TEMP/MO_H_TEMP_2022.txt'
 # download CHLA-datasets and process them
 # input-params: url of dataset, sql statement for generating table, type of dataset (CHLA or temp)  
 statement_CHLA="CREATE TABLE IF NOT EXISTS CHLA (Cruise VARCHAR(255), Station VARCHAR(255), Type VARCHAR(10), Date TEXT, [Longitude degrees_east] REAL, [Latitude degrees_north] REAL, LOCAL_CDI_ID VARCHAR(255), EDMO_code VARCHAR(10), [Bot Depth in m] FLOAT, [Depth in m] FLOAT, QV INTEGER, [CHLA_FLM_FGN_ALC in µg/l] FLOAT, QV_1 INTEGER, [CHLA_PTM_F_ACE in µg/l]	FLOAT, QV_2 INTEGER, [CHLA_SENE_DEV_NDT in µg/l] FLOAT, QV_3 INTEGER, [CHLA_SENE_NOT_NDT in  µg/l] FLOAT, QV_4 INTEGER)"
-statement_TEMP="CREATE TABLE IF NOT EXISTS TEMP (Cruise VARCHAR(255), Station VARCHAR(255), Type VARCHAR(10), Date TEXT, [Longitude degrees_east] REAL, [Latitude degrees_north] REAL, LOCAL_CDI_ID VARCHAR(255), EDMO_code VARCHAR(10), [Bot Depth in m] FLOAT, [Depth in m] FLOAT, QV INTEGER, [CHLA_FLM_FGN_ALC in µg/l] FLOAT, QV_1 INTEGER, [TEMP_THEPRC_DEV_NDT[°C]]	FLOAT, QV_2 INTEGER)"
+statement_TEMP="CREATE TABLE IF NOT EXISTS TEMP (Cruise VARCHAR(255), Station VARCHAR(255), Type VARCHAR(10), Date TEXT, [Longitude degrees_east] REAL, [Latitude degrees_north] REAL, LOCAL_CDI_ID VARCHAR(255), EDMO_code VARCHAR(10), [Bot Depth in m] FLOAT, [Depth in m] FLOAT, QV INTEGER, [CHLA_FLM_FGN_ALC in µg/l] FLOAT, QV_1 INTEGER, [TEMP_THEPRC_DEV_NDT in °C]	FLOAT, QV_2 INTEGER)"
+# parallelize it
 download_and_process(url1, statement_CHLA, "CHLA")
 download_and_process(url2, statement_CHLA, "CHLA")
 download_and_process(url3, statement_TEMP, "TEMP")
