0bd373eff2e5e63711cd22590428e3e04f221ce4
kreisligaspieler <e.pfann@gmx.de>
2023-05-21 16:51:09 +0200

    exercise1

---
+++ exercises/exercise1.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///./exercises/airports.sqlite', echo=False)
+
+df = pd.read_csv("https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv",sep=';') 
+df.to_sql("airports", con=engine)
