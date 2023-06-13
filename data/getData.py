665308fea2ae2847def8914e32c27aec45732c8e
kreisligaspieler <e.pfann@gmx.de>
2023-06-14 00:14:42 +0200

    Continued exploration, getData optimized

--- data/getData.py
+++ data/getData.py
@@ -6,17 +6,17 @@ from sqlalchemy import create_engine
 
 
 engine = create_engine('sqlite:///../data/data.sqlite')
-df = pd.read_csv("https://data.bsh.de/OpenData/DOD/MO_H_CHLA/MO_H_CHLA_2018.txt",sep='\t',skiprows=103, encoding='ISO-8859-15', skip_blank_lines=True, header=0, names=["Cruise", "Station","Type", "Date", "Longitude [degrees_east]", "Latitude [degrees_north]", "LOCAL_CDI_ID","EDMO_code", "Bot Depth [m]", "Depth [m]", "QV_1", "CHLA_FLM_FGN_AL [µg/l]", "QV_2", "CHLA_SENE_DEV_NDT [µg/l]", "QV_3", " "],usecols=range(15))
+df = pd.read_csv("https://data.bsh.de/OpenData/DOD/MO_H_CHLA/MO_H_CHLA_2018.txt",sep='\t',skiprows=103, encoding='ISO-8859-15', skip_blank_lines=True, header=0, names=["Cruise", "Station","Type", "Date", "Longitude [degrees_east]", "Latitude [degrees_north]", "LOCAL_CDI_ID","EDMO_code", "Bot Depth [m]", "Depth [m]", "QV_1", "CHLA_FLM_FGN_AL [µg/l]", "QV_2", "CHLA_SENE_DEV_NDT [µg/l]", "QV_3", " "],usecols=range(15), decimal=',')
 df.to_sql("CHLA_18", con=engine, index=False)
 
-df = pd.read_csv("https://data.bsh.de/OpenData/DOD/MO_H_CHLA/MO_H_CHLA_2022.txt",sep='\t',skiprows=91, encoding='ISO-8859-15', skip_blank_lines=True, header=0, names=["Cruise", "Station","Type", "Date", "Longitude [degrees_east]", "Latitude [degrees_north]", "LOCAL_CDI_ID","EDMO_code", "Bot Depth [m]", "Depth [m]", "QV_1", "CHLA_SENE_DEV_NDT [µg/l]", "QV_2"],usecols=range(13))
+df = pd.read_csv("https://data.bsh.de/OpenData/DOD/MO_H_CHLA/MO_H_CHLA_2022.txt",sep='\t',skiprows=91, encoding='ISO-8859-15', skip_blank_lines=True, header=0, names=["Cruise", "Station","Type", "Date", "Longitude [degrees_east]", "Latitude [degrees_north]", "LOCAL_CDI_ID","EDMO_code", "Bot Depth [m]", "Depth [m]", "QV_1", "CHLA_SENE_DEV_NDT [µg/l]", "QV_2"],usecols=range(13), decimal=',')
 df.to_sql("CHLA_22", con=engine, index=False)
 
 
-df = pd.read_csv("https://data.bsh.de/OpenData/DOD/MO_H_TEMP/MO_H_TEMP_2018.txt",sep='\t',skiprows=103, encoding='ISO-8859-15', skip_blank_lines=True, header=0, names=["Cruise", "Station","Type", "Date", "Longitude [degrees_east]", "Latitude [degrees_north]", "LOCAL_CDI_ID","EDMO_code", "Bot Depth [m]", "Depth [m]", "QV_1", "TEMP_THE_NOT_NDT[°C]", "QV_2", "TEMP_THEPRC_NOT_NDT[°C]", "QV_3"],usecols=range(15))
+df = pd.read_csv("https://data.bsh.de/OpenData/DOD/MO_H_TEMP/MO_H_TEMP_2018.txt",sep='\t',skiprows=103, encoding='ISO-8859-15', skip_blank_lines=True, header=0, names=["Cruise", "Station","Type", "Date", "Longitude [degrees_east]", "Latitude [degrees_north]", "LOCAL_CDI_ID","EDMO_code", "Bot Depth [m]", "Depth [m]", "QV_1", "TEMP_THE_NOT_NDT[°C]", "QV_2", "TEMP_THEPRC_NOT_NDT[°C]", "QV_3"],usecols=range(15), decimal=',')
 df.to_sql("TEMP_18", con=engine, index=False)
 
-df = pd.read_csv("https://data.bsh.de/OpenData/DOD/MO_H_TEMP/MO_H_TEMP_2022.txt",sep='\t',skiprows=91, encoding='ISO-8859-15', skip_blank_lines=True, header=0, names=["Cruise", "Station","Type", "Date", "Longitude [degrees_east]", "Latitude [degrees_north]", "LOCAL_CDI_ID","EDMO_code", "Bot Depth [m]", "Depth [m]", "QV_1", "TEMP_THE_NOT_NDT[°C]", "QV_2"],usecols=range(13))
+df = pd.read_csv("https://data.bsh.de/OpenData/DOD/MO_H_TEMP/MO_H_TEMP_2022.txt",sep='\t',skiprows=91, encoding='ISO-8859-15', skip_blank_lines=True, header=0, names=["Cruise", "Station","Type", "Date", "Longitude [degrees_east]", "Latitude [degrees_north]", "LOCAL_CDI_ID","EDMO_code", "Bot Depth [m]", "Depth [m]", "QV_1", "TEMP_THE_NOT_NDT[°C]", "QV_2"],usecols=range(13), decimal=',')
 df.to_sql("TEMP_22", con=engine, index=False)  
 
  
