662429d987689ba2e9ada956e213f9f5dcdc9628
kreisligaspieler <97282769+kreisligaspieler@users.noreply.github.com>
2023-06-14 12:06:31 +0200

    Update checkData.py

--- data/checkData.py
+++ data/checkData.py
@@ -11,5 +11,7 @@ def checkRows(table, numRows):
         
 if checkRows("CHLA_18", 466) and checkRows("CHLA_22", 82938) and checkRows("TEMP_18", 50574) and checkRows("TEMP_22", 82930):
     print("Data successfully imported into database.")
+else:
+    raise Exception("Data not successfully imported!")
 
 
