72705452c36a478021e6b67608ee58b1905eb11f
Elias <elias.pfann@fau.de>
2023-06-23 10:47:13 +0200

    check data adapted to dynamical length of 2022 data, working on report

--- data/checkData.py
+++ data/checkData.py
@@ -4,12 +4,17 @@ def checkRows(table, numRows):
     con = sqlite3.connect("../data/data.sqlite")
     cur = con.cursor()
     rows = cur.execute("SELECT COUNT(*) FROM "+table)
-    if not rows.fetchone() == (numRows,):
+    # Tables from 22 still got updated at the creation of the report
+    if table == "TEMP_22" or table == "CHLA_22":
+        if rows.fetchone() < (numRows,):
+            print(table+": Not all rows hav been imported into database!")
+            return False
+    elif not rows.fetchone() == (numRows,):
         print(table+": Not all rows hav been imported into database!")
         return False
     return True
         
-if checkRows("CHLA_18", 466) and checkRows("CHLA_22", 82938) and checkRows("TEMP_18", 50574) and checkRows("TEMP_22", 82930):
+if checkRows("CHLA_18", 466) and checkRows("CHLA_22", 82938) and checkRows("TEMP_18", 50574) and checkRows("TEMP_22",144628):
     print("Data successfully imported into database.")
 else:
     raise Exception("Data not successfully imported!")
